6fe2551bb55dfc17c99065d347945f1732fb2057
FOXYCRON <57571091+FOXYCRON@users.noreply.github.com>
2022-06-24 12:24:59 -0600

    Add files via upload

---
+++ checkbox.php
@@ -0,0 +1,20 @@
+<html>
+   <head>
+         <title> Checkbox </title>
+   </head>
+     <body>
+        <form method = "post" action = "ChekOperaciones2.php">
+           <p> Ingrese primer valor: </p>
+              <input type = "text" name = "valor1">
+               <br>
+           </p> Insgrese segundo valor: </p>
+                <input type = "text" name = "valor2">
+                <br>
+                <input type = "checkbox" name = "check1"> Sumar
+             <br>
+                 <input type = "checkbox" name = "check2"> Restar
+             <br>
+                <input type = "submit" name = "operar">        
+         </form>
+    </body>
+</html>
